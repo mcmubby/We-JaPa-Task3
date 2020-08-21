b014ff63f75833d3ec41d18633074033d3b43f17
Mubarak Ayelotan <12859241+mcmubby@users.noreply.github.com>
2020-08-21 12:26:16 +0100

    hardcoded filter

--- filter.js
+++ filter.js
@@ -10,8 +10,18 @@ function replacer(name, val) {
     
 };
 
+function arrayCleaner(dClean){
+    for (let i = 0; i < dClean.length; i++) {
+        if(dClean[i] === '' || dClean[i]==='-' || dClean[i] ==='N/A'){
+            dClean.splice(i,1);
+            return dClean;
+        }
+    }
+}
+
 cleanData = (pData) => {
     
+    arrayCleaner(pData.hobbies);
     fs.writeFile(path.join(__dirname, 'Response.txt'), JSON.stringify(pData, replacer, 2), (err)=>{
         if(err){console.log(err);}
     })
